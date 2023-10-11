2667d84370407d070b8d7511c1e580a635e07847
Bryan Emmett <bryan.emmett@ceres.scinet.usda.gov>
2023-10-11 15:22:11 -0500

    Initial Processing

---
+++ notebooks/ITS2/batch_cutadapt.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# job standard output will go to the file slurm-%j.out (where %j is the job ID)
+
+#SBATCH --time=6:00:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=2   # 1 processor core(s) per node X 2 threads per core
+#SBATCH --mem=6G   # maximum memory per node
+#SBATCH --partition=short    # standard node(s)
+#SBATCH --job-name="cutadapt"
+#SBATCH --mail-user=bryan.emmett@usda.gov   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+############### user inputs
+
+# specify paths of input and output folders
+dir='/project/mmicrobe/Kelley/ITS2/Raw/KY21-3-4/raw'
+filtdir='filtN'
+filtdir="$dir/$filtdir"
+
+outdir='cutadapt'
+dir2="$dir/$outdir"
+
+unTdir="untrimmed"
+dir3="$dir/$unTdir"
+
+# Specify primer sequences
+
+FWD="AACTTTYRRCAAYGGATCWCT"
+REV="AGCCTCCGCTTATTGATATGCTTAART"
+FWDrc="AGWGATCCRTTGYYRAAAGTT"
+REVrc="AYTTAAGCATATCAATAAGCGGAGGCT"
+
+##################
+
+module load cutadapt
+
+# Check for output directory and if not present, make directory
+
+if [ ! -d $filtdir ]; then
+  mkdir -p $filtdir;
+fi
+
+if [ ! -d $dir3 ]; then
+  mkdir -p $dir3;
+fi
+
+
+# for loop to find all files with R1 in name, change their name to R2 and write out new filename to a new file in a new directory
+
+for r1 in $filtdir/*_R1_*; do
+
+r2=${r1/_R1_/_R2_}
+
+baseR1=${r1##*/}
+baseR2=${r2##*/}
+
+
+fnFsCut="$dir2/$baseR1"
+fnRsCut="$dir2/$baseR2"
+untrimmedF="$dir3/untrimmed.$baseR1"
+untrimmedR="$dir3/untrimmed.$baseR2"
+
+
+#echo $baseR2
+#echo $dir3
+#echo  $untrimmedR
+
+cutadapt -g $FWD \
+	-a $REVrc \
+	-G $REV \
+	-A $FWDrc \
+	-n 2 \
+	--untrimmed-output=$untrimmedF \
+	--untrimmed-paired-output=$untrimmedR \
+	-o $fnFsCut \
+	-p $fnRsCut \
+	$r1 \
+	$r2
+done
